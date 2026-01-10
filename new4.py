a333add69c23df4f17a4db967588f90bf845d8e3
p10prmar <p.10prmar@gmail.com>
2026-01-10 13:57:40 +0530

    Update new4.py

--- new4.py
+++ new4.py
@@ -3,7 +3,7 @@
 VAST - Vulnerability Assessment & Scanning Tool
 """
 
-import sys
+impor sys
 import os
 import requests
 import re
@@ -1451,4 +1451,5 @@ if __name__ == "__main__":
     vast = VAST()
 
     vast.run()
+
 
